81b25a6727fcee2585ced01ca5e35699815d47fa
suraj paul <paulsuraj044@gmail.com>
2025-01-17 16:36:05 +0000

    Designing Landing Page

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Hero() {
     return (
-        <div>
-            Hero
+        <div className='flex items-center mx-56 gap-9'>
+            <h1 className='font-extrabold text-[60px] text-center mt-16'>
+                <span className='text-[#f56551]'>Discover Your Next Adventure with AI:</span> <br />  Personalized Itineraries at Your Fingertips</h1>
+            <p>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
         </div>
     )
 }
