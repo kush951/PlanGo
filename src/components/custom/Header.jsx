7f6d9d76d8a4c3d0bfdf11275359dd29888e158c
suraj paul <paulsuraj044@gmail.com>
2025-01-17 16:27:20 +0000

    logo added to Header

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>Header</div>
+        <div>
+            <img src="/logo.svg" alt="logo" />
+        </div>
     )
 }
 export default Header;
