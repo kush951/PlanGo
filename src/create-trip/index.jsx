4d0a509180f632071c841ce2e7d30e36d58f14c8
suraj paul <paulsuraj044@gmail.com>
2025-01-19 05:36:45 +0000

    fixing autocomplete

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,24 @@
+
 import React from 'react'
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 function CreateTrip() {
     return (
-        <div>
-            <h2>CreateTrip</h2>
+        <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+            <h2 className='font-bold text-3xl'>Tell us your travel preferences 🏕️🌴</h2>
+            <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
+
+            <div className='mt-20'>
+                <div>
+                    <h2 className='text-xl my-3 font-medium'>Destination?</h2>
+                    <GooglePlacesAutocomplete
+                        apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+                        selectProps={{
+
+                        }}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
