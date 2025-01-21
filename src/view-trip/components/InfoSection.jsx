970abc0a43028b9023c4438d95e5660958e3fb4b
suraj paul <paulsuraj044@gmail.com>
2025-01-21 12:50:03 +0000

    info section

--- src/view-trip/components/InfoSection.jsx
+++ src/view-trip/components/InfoSection.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import React from 'react';
+import { IoIosSend } from "react-icons/io";
 
-function InfoSection() {
+function InfoSection({ trip }) {
     return (
         <div>
             <img src="/placeholder.webp" className='h-[340px] w-full object-cover rounded-xl' />
+            <div className='flex justify-between items-center'>
 
-            <div>
+                <div className='my-5 flex flex-col gap-2'>
+                    <h2 className='font-bold text-2xl'>{trip?.userSelection?.destination}</h2>
+                    <div className='flex gap-5'>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md'>📅 {trip?.userSelection?.noOfDays} Day</h2>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md'>💵 {trip?.userSelection?.budget} Budget</h2>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md'>🥂 No. of Traveller: {trip?.userSelection?.traveler} </h2>
 
+                    </div>
+                </div>
+                <Button><IoIosSend /></Button>
             </div>
 
         </div>
