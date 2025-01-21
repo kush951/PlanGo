b0612ccc5909684e65a681e540e76cdc9abe16ba
suraj paul <paulsuraj044@gmail.com>
2025-01-21 14:26:57 +0000

    hotels fetch data

--- src/view-trip/components/Hotels.jsx
+++ src/view-trip/components/Hotels.jsx
@@ -5,11 +5,21 @@ function Hotels({ trip }) {
         <div>
             <h2 className='font-bold text-xl mt-5'>Hotel Recommendation</h2>
 
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grids-col-4'>
-                {trip?.tripData?.hotelOptions?.map((item, index) => (
-                    <div>
+            <div className='grid grid-cols-2 md:grid-cols-3 xl:grids-col-4 gap-5'>
+                {trip?.tripData?.hotelOptions?.map((hotel, index) => (
+                    <div className='hover:scale-105 transition-all cursor-pointer'>
                         <img src="/placeholder.webp" className='rounded-xl' />
+
+                        <div className='my-2 flex flex-col gap-2'>
+                            <h2 className='font-medium'>{hotel?.hotelName}</h2>
+                            <h2 className='text-xs text-gray-500'>📍 {hotel?.hotelAddress}</h2>
+                            <h2 className='text-sm'>💵 {hotel?.price}</h2>
+                            <h2 className='text-sm'>⭐ {hotel?.rating}</h2>
+
+                        </div>
+
                     </div>
+
                 ))}
             </div>
         </div>
