b5d14d2207da457d7a047ccc981e661fac289b07
suraj paul <paulsuraj044@gmail.com>
2025-01-21 11:59:47 +0000

    info from firebase

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { db } from '@/service/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { toast } from 'sonner';
+
 
 function Viewtrip() {
+
+    const { tripId } = useParams();
+
+    const GetTripData = async () => {
+        const docRef = doc(db, 'AITrips', tripId);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists) {
+            console.log("Document", docSnap.data());
+        } else {
+            console.log("No such Document");
+            toast("No trip Found");
+        }
+    }
+
+    useEffect(() => {
+        tripId && GetTripData();
+    }, [tripId])
+
+    // use to get Trip Info from firebase
+
     return (
         <div>
-            Viewtripe
+            Viewtripe: {tripId}
         </div>
     )
 }
