6009c4f17663b42d960294bd0e7073583aa47d85
suraj paul <paulsuraj044@gmail.com>
2025-01-21 12:21:53 +0000

    infosection and placeholder fixed

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -1,36 +1,38 @@
-import { db } from '@/service/firebaseConfig';
-import { doc, getDoc } from 'firebase/firestore';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'; // Added useState import
 import { useParams } from 'react-router-dom';
 import { toast } from 'sonner';
-
+import { db } from '@/service/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
+import InfoSection from '../components/InfoSection';
 
 function Viewtrip() {
-
     const { tripId } = useParams();
+    const [trip, setTrip] = useState([]); // Corrected the state variable name from 'tript' to 'trip'
 
     const GetTripData = async () => {
         const docRef = doc(db, 'AITrips', tripId);
         const docSnap = await getDoc(docRef);
-        if (docSnap.exists) {
+        if (docSnap.exists()) {
             console.log("Document", docSnap.data());
+            setTrip(docSnap.data());
         } else {
             console.log("No such Document");
             toast("No trip Found");
         }
-    }
+    };
 
     useEffect(() => {
         tripId && GetTripData();
-    }, [tripId])
-
-    // use to get Trip Info from firebase
+    }, [tripId]);
 
     return (
-        <div>
-            Viewtripe: {tripId}
+        <div className='p-10 md:px-20 lg:px-44 xl:px-56'>
+            {/* Information Section */}
+            <InfoSection trip={trip} />
+            {/* Recommended Hotels */}
+            {/* Daily Plan */}
         </div>
-    )
+    );
 }
 
 export default Viewtrip;
---
+++ src/view-trip/components/InfoSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function InfoSection() {
+    return (
+        <div>
+            <img src="/placeholder.webp" className='h-[340px] w-full object-cover rounded-xl' />
+
+            <div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default InfoSection;
