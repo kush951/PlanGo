2364bacde1b11c6373aad5b36260c20a73febd17
suraj paul <paulsuraj044@gmail.com>
2025-01-24 13:02:27 +0000

    change

--- src/my-trips/index.jsx
+++ src/my-trips/index.jsx
@@ -35,13 +35,16 @@ function MyTrips() {
         }
     };
 
+    const handleDelete = (tripId) => {
+        setUserTrips((prevTrips) => prevTrips.filter((trip) => trip.id !== tripId));
+    };
+
     return (
         <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
             <h2 className='font-bold text-3xl'>My Trips</h2>
             <div className='grid grid-cols-2 md:grid-cols-3 gap-5'>
                 {userTrips.map((trip) => (
-                    <UserTripComponent key={trip.id} trip={trip}
-                    />
+                    <UserTripComponent key={trip.id} trip={trip} onDelete={handleDelete} />
                 ))}
             </div>
         </div>
