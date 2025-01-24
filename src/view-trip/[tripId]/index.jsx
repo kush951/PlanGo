81f3a0e0482464cb79dede547d08b3ac40ed9c6c
suraj paul <paulsuraj044@gmail.com>
2025-01-24 08:26:08 +0000

    view-trip responsive

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -35,7 +35,7 @@ function Viewtrip() {
     }
 
     return (
-        <div className='p-10 md:px-20 lg:px-44 xl:px-56'>
+        <div className='p-4 sm:p-6 md:p-10 lg:px-20 xl:px-44 2xl:px-56'>
             {/* Information Section */}
             <InfoSection trip={trip} />
             {/* Recommended Hotels */}
